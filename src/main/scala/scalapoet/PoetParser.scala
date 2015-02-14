f4148f7a48fa3e2acf5dddce5291b88a3746d966
Steven Ellis <steven.ellis@mademedia.co.uk>
2015-02-14 20:21:35 +0000

    Fleshed out parser so it can produce the suffix map and provide context for a word

--- src/main/scala/scalapoet/PoetParser.scala
+++ src/main/scala/scalapoet/PoetParser.scala
@@ -1,10 +1,39 @@
 package scalapoet
 
-class PoetParser {
+import java.util.Random
 
-  def createTextList(input: String): List[String] = {
+class PoetParser(input: String) {
+
+  val suffixMapper = new SuffixMapper
+  val wordListWithPunctuation = input.replaceAll("\n", " ").split(" ").toList
+  val wordListWithoutPunctuation = createTextList
+  val rand = new Random(System.currentTimeMillis());
+
+  def createTextList(): List[String] = {
     val charsToIgnore = "!,.?".toSet
-    input.filterNot(charsToIgnore).split(" ").toList
+    input.filterNot(charsToIgnore)
+      .replaceAll("\n", " ")
+      .split(" ")
+      .toList
   }
 
+  def createSuffixMap(): Map[String, List[String]] = suffixMapper.map(createTextList())
+
+  def getContextForWord(word: String): String = {
+    val wordOccurrences = allOccurrencesOf(wordListWithoutPunctuation, word, 1)
+    val wordPos = wordOccurrences(rand.nextInt(wordOccurrences.length))
+    val line = wordListWithPunctuation.slice(wordPos - 4, wordPos + 1).mkString(" ")
+
+    return formatLine(line)
+  }
+
+  private def formatLine(line: String): String = line(0).toUpper + line.substring(1)
+
+  private def allOccurrencesOf(input: List[String], txt: String, from: Int): List[Int] = {
+    val idx = input.indexOf(txt, from)
+    if (idx == -1)
+      List()
+    else
+      List(idx) ++ allOccurrencesOf(input, txt, idx + 1)
+  }
 }
--- src/test/scala/scalapoet/PoetParserSuite.scala
+++ src/test/scala/scalapoet/PoetParserSuite.scala
@@ -8,26 +8,66 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class PoetParserSuite extends FunSuite {
 
-  val poetParser = new PoetParser()
-
   test("can turn input string into a list of words") {
     val input = "this is a test"
+    val poetParser = new PoetParser(input)
     val output = List("this", "is", "a", "test")
 
-    assert(poetParser.createTextList(input) === output)
+    assert(poetParser.createTextList() === output)
   }
 
   test("it ignores punctuation") {
     val input = "this. is, a!, test?"
+    val poetParser = new PoetParser(input)
     val output = List("this", "is", "a", "test")
 
-    assert(poetParser.createTextList(input) === output)
+    assert(poetParser.createTextList() === output)
   }
-  
+
   test("it splits across multiple lines") {
     val input = "this is\na test"
+    val poetParser = new PoetParser(input)
     val output = List("this", "is", "a", "test")
 
-    assert(poetParser.createTextList(input) === output)
+    assert(poetParser.createTextList() === output)
+  }
+
+  test("it can create a suffix map from an input string") {
+    val input = "Some bland information about the band demands an explanation"
+    val poetParser = new PoetParser(input)
+    val output = Map("and" -> List("bland", "band"), "ation" -> List("information", "explanation"))
+
+    assert(poetParser.createSuffixMap() === output)
+  }
+
+  test("it can retrieve the context for a word") {
+    val input = "Some bland information about the band demands an explanation"
+    val poetParser = new PoetParser(input)
+
+    val output = "The band demands an explanation"
+
+    assert(poetParser.getContextForWord("explanation") === output)
+  }
+
+  test("it can retrieve the context for a word that appears multiple times") {
+    val input = "This is some info about the band and this is unrelated to the band"
+    val poetParser = new PoetParser(input)
+    
+    val possibleOutput1 = "Some info about the band"
+    val possibleOutput2 = "Is unrelated to the band"  
+      
+    var foundOne = false
+    var foundTwo = false
+    
+    for(x <- 1 to 10) {
+      val output = poetParser.getContextForWord("band")
+      if (output == possibleOutput1) {
+        foundOne = true
+      } else if (output == possibleOutput2) {
+        foundTwo = true
+      }
+    }
+    assert(foundOne === true, "Did not find output 1")
+    assert(foundTwo === true, "Did not find output 2")
   }
 }
